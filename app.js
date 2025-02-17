803d11c0d28676770ec3d45a21624ea9d80ae525
kesrishubham2510 <shubhamkeshari90@gmail.com>
2025-02-17 16:38:52 +0530

    Refactored | Added middleware to handle requests

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const http = require('http');
 
-const requestHandler = require('./routes');
+const express = require('express');
+
+// because express package exports a function 
+const app = express();
+
+// Adding the middleware
+app.use((req, res, next)=>{
+    console.log("This is my first middleware");
+    next();
+})
+
+app.use((req, res, next)=>{
+    res.send("<h1>! Hello from a middleware !</h1>");
+})
 
 /* 
     -> createServer accepts a event listener
     -> This eventListener is executed for every incoming request
 */
 
-const server = http.createServer(requestHandler);
+const server = http.createServer(app);
 
 server.listen(3000);
