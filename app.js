9fb780a7ef7e269fa392077034bb1228c0860df3
kesrishubham2510 <shubhamkeshari90@gmail.com>
2025-02-17 17:17:33 +0530

    Parsed body to log incoming data

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const http = require('http');
+const bodyParser = require('body-parser');
 
 const express = require('express');
 
 // because express package exports a function 
 const app = express();
 
-// Adding the middleware
-app.use((req, res, next)=>{
-    console.log("This is my first middleware");
-    next();
-})
+// Middleware to parse the requestBody
+// false means, parse the incoming data as simple objects like array, String
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next)=>{
+// Adding the middleware
+app.use('/contact',(req, res, next)=>{
+    res.send('<!DOCTYPE html> <html> <head> <title>Contact Form</title> </head> <body> <h2>Contact Form</h2> <form action="/add-contact" method="POST"> <label for="name">Name:</label><br> <input type="text" id="name" name="name" required><br><br> <button type="submit">Send</button> </form> </body> </html>');
+});
+
+// Adding the middleware to add the contact
+app.use('/add-contact',(req, res, next)=>{
+    console.log(req.body);
+    res.send('<html><head></head><body><h1>! Contact stored !</h1></body></html>')
+});
+
+app.use('/', (req, res, next)=>{
     res.send("<h1>! Hello from a middleware !</h1>");
-})
+});
 
 /* 
     -> createServer accepts a event listener
